e5d0ffd4c1ff0e4dfb1af515a1b6ee8048b2dd36
Kerrick Staley <kerrick@kerrickstaley.com>
2023-11-06 22:26:11 -0500

    Make it compatible with older bluetoothctl versions
    The Steam Deck only has bluetoothctl 5.63, which does not support the
    "bluetoothctl devices Connected" command.

--- usr/lib/systemd/system-sleep/restore-bluetooth-connection.py
+++ usr/lib/systemd/system-sleep/restore-bluetooth-connection.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+import errno
+import os
 import re
 import subprocess
 import sys
@@ -9,36 +11,51 @@ argparser.add_argument(
     'transition', help='either "pre" or "post"',
     choices=['pre', 'post'])
 
+LAST_DEVICE_FILE = '/tmp/last-bluetooth-device'
 
 def get_connected_device_mac_addresses():
     ret = []
-    output = subprocess.check_output([
-        'bluetoothctl',
-        'devices',
-        'Connected',
-    ]).decode()
+    try:
+        output = subprocess.check_output([
+            'bluetoothctl',
+            'info',
+        ]).decode()
+    except subprocess.CalledProcessError:
+        # happens when e.g. Bluetooth is disabled
+        return ret
 
     for line in output.splitlines():
+        if not line.startswith('Device '):
+            continue
         ret.append(line.split()[1])
 
     return ret
 
+
 def main(args):
     if args.transition == 'pre':
         devices = get_connected_device_mac_addresses()
-        with open('/tmp/last-bluetooth-device', 'w') as f:
-            if len(devices) == 1:
+
+        try:
+            os.remove(LAST_DEVICE_FILE)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
+
+        if len(devices) == 1:
+            with open(LAST_DEVICE_FILE, 'w') as f:
                 f.write(devices[0] + '\n')
+
     elif args.transition == 'post':
         try:
-            with open('/tmp/last-bluetooth-device') as f:
+            with open(LAST_DEVICE_FILE) as f:
                 device = f.read().strip()
         except FileNotFoundError:
             return
 
         if not re.fullmatch(r'([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}', device):
             raise ValueError(f'invalid device MAC address {device}')
-        
+
         subprocess.check_call([
             'bluetoothctl',
             'connect',
