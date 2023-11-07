0deca76042abe9886071d3bca078ff3b7b7b69c9
Kerrick Staley <kerrick@kerrickstaley.com>
2023-11-07 00:08:41 -0500

    Fix several bugs
    - Fix argparse parsing (need to handle "event" argument)
    - Repeatedly retry connecting to the device for up to 10 seconds
    - Upon exception, write the exception to a log file

--- usr/lib/systemd/system-sleep/restore-bluetooth-connection.py
+++ usr/lib/systemd/system-sleep/restore-bluetooth-connection.py
@@ -5,13 +5,15 @@ import os
 import re
 import subprocess
 import sys
+import time
+import traceback
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument(
-    'transition', help='either "pre" or "post"',
-    choices=['pre', 'post'])
+argparser.add_argument('prepost', choices=['pre', 'post'])
+argparser.add_argument('event')
 
 LAST_DEVICE_FILE = '/tmp/last-bluetooth-device'
+CONNECT_RETRY_SECS = 10
 
 def get_connected_device_mac_addresses():
     ret = []
@@ -33,7 +35,10 @@ def get_connected_device_mac_addresses():
 
 
 def main(args):
-    if args.transition == 'pre':
+    if args.event != 'suspend':
+        return
+
+    if args.prepost == 'pre':
         devices = get_connected_device_mac_addresses()
 
         try:
@@ -46,7 +51,7 @@ def main(args):
             with open(LAST_DEVICE_FILE, 'w') as f:
                 f.write(devices[0] + '\n')
 
-    elif args.transition == 'post':
+    elif args.prepost == 'post':
         try:
             with open(LAST_DEVICE_FILE) as f:
                 device = f.read().strip()
@@ -56,14 +61,25 @@ def main(args):
         if not re.fullmatch(r'([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}', device):
             raise ValueError(f'invalid device MAC address {device}')
 
-        subprocess.check_call([
-            'bluetoothctl',
-            'connect',
-            device,
-        ])
+        start_time = time.time()
+        while time.time() < start_time + CONNECT_RETRY_SECS:
+            try:
+                subprocess.check_call([
+                    'bluetoothctl',
+                    'connect',
+                    device,
+                ])
+            except:
+                time.sleep(0.1)
+
     else:
         raise RuntimeError('unreachable')
 
 
 if __name__ == '__main__' and not hasattr(sys, 'ps1'):
-    main(argparser.parse_args())
+    try:
+        main(argparser.parse_args())
+    except:
+        with open('/tmp/exception', 'w') as f:
+            traceback.print_exc(file=f)
+        raise
